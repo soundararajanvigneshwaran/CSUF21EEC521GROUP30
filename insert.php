a7bc39522ec4bd743505652a4bc7598fe8bd25d0
soundararajanvigneshwaran <91158418+soundararajanvigneshwaran@users.noreply.github.com>
2021-11-21 17:30:30 -0500

    Customer feedbacks
    Customer feedbacks stored into database(MYSQL), so users can submit their feedbacks.

--- insert.php
+++ insert.php
@@ -1,12 +1,53 @@
 <?php
-$name =$_POST['customername']
-$password =$_POST['password']
-$review =$_POST['reviews']
-if(!empty($name)||!empty($password)||!empty($reviews))
-{
-    $host = "localhost";
-    $dbusername ="root";
-    $dbpassword ="admin";
-    $dbname="Dividefeedback"
+if (isset($_POST['submit'])) {
+		if (isset($_POST['firstname']) && isset($_POST['lastname']) &&
+        isset($_POST['emailid']) && isset($_POST['phonenumber']) &&
+        isset($_POST['member']) && isset($_POST['response'])) {
+        
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $emailid = $_POST['emailid'];
+        $phonenumber = $_POST['phonenumber'];
+        $member = $_POST['member'];
+        $response = $_POST['response'];
+        $host = "localhost";
+        $dbUsername = "root";
+        $dbPassword = "";
+        $dbName = "restaurant";
+        $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+        if ($conn->connect_error) {
+            die('Could not connect to the database.');
+        }
+        else {
+            $SELECT = "SELECT emailid FROM feedback WHERE emailid = ? LIMIT 1";
+            $INSERT = "INSERT INTO feedback(firstname, lastname, emailid, phonenumber, member, response) values(?, ?, ?, ?, ?, ?)";
+			$stmt = $conn->prepare($SELECT);
+            $stmt->bind_param("s", $emailid);
+            $stmt->execute();
+            $stmt->bind_result($emailid);
+            $stmt->store_result();
+            $rnum = $stmt->num_rows;
+            if ($rnum == 0) {
+                $stmt->close();
+                $stmt = $conn->prepare($INSERT);
+                $stmt->bind_param("sssiss",$firstname, $lastname, $emailid, $phonenumber, $member, $response);
+                $stmt->execute();
+                    echo "New record inserted sucessfully.";
+                }
+                else {
+                echo "Someone already registers using this email.";
+            }
+            $stmt->close();
+            $conn->close();
+        }
+    }
+    else {
+        echo "All field are required.";
+        die();
+    }
 }
+else {
+    echo "Submit button is not set";
+}
+
 ?>
